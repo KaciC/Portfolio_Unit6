4808cde0e362b8bbd8d5440650082c08e8e0e26f
Kaci Childers <kaci.childers@gmail.com>
2017-01-09 23:11:52 +0000

    I changed stuff

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
     //Write all your jQuery code here
         $("button").click(function() {
-      $("#like").html("You like this project!");
+      $("#like1").html("You like this project!");
+    });
+    
+    
+    
+    
+});
+$(document).ready(function(){
+    //Write all your jQuery code here
+        $("button").click(function() {
+      $("#like2").html("You like this project!");
     });
     
     
